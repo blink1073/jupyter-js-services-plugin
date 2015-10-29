59c75dbce2ceb9ed47733b953457c093ed827d70
Steven Silvester <steven.silvester@ieee.org>
2015-10-29 10:54:09 -0500

    Get a clean build

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
 
-import { Config } from 'jupyter-js-services';
+import { getConfigSection, ConfigWithDefaults } from 'jupyter-js-services';
 
--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,3 @@
 // Distributed under the terms of the Modified BSD License.
 'use strict';
 
-export * from './config';
-export * from './contents';
-export * from './ikernel';
-export * from './isession';
-export * from './kernel';
-export * from './session';
-export { IAjaxOptions } from './utils';
