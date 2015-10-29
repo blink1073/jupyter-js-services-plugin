b2fb773c9f06f6a0c92ea9756812f09acd7bbaa5
Steven Silvester <steven.silvester@ieee.org>
2015-10-29 09:39:24 -0500

    Add front matter

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Copyright (c) Jupyter Development Team.
+// Distributed under the terms of the Modified BSD License.
+
+import { Config } from 'jupyter-js-services';
+
---
+++ src/kernel.ts
@@ -0,0 +1,10 @@
+// Copyright (c) Jupyter Development Team.
+// Distributed under the terms of the Modified BSD License.
+'use strict';
+
+import { 
+  getKernelSpecs, listRunningKernels, startNewKernel, connectToKernel,
+  createKernelMessage
+} from 'jupyter-js-services';
+
+
